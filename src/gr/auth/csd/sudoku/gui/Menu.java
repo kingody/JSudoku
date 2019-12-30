29d6c2fde690dbe1b8af96d20bd77dcd5dbd04c5
kingody <kingody2000@gmail.com>
2019-12-30 16:56:29 +0200

    Fixed stupid bug

--- src/gr/auth/csd/sudoku/gui/Menu.java
+++ src/gr/auth/csd/sudoku/gui/Menu.java
@@ -56,7 +56,7 @@ public class Menu extends JFrame {
     }
 
     public void setCharSet(int index) {
-        if (index > 0 && index < charSet.length)
+        if (index >= 0 && index < charSet.length)
             current = charSet[index];
     }
 
