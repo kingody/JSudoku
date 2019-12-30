67e05c79f09cffdfc8f924e4cb6c596b6db1b69e
kingody <kingody2000@gmail.com>
2019-12-30 15:42:24 +0200

    Optimised Settings Window

--- src/gr/auth/csd/sudoku/gui/SettingsWindow.java
+++ src/gr/auth/csd/sudoku/gui/SettingsWindow.java
@@ -22,24 +22,19 @@ public class SettingsWindow extends JFrame{
     public SettingsWindow(Menu menu){
         super("Settings");
 
-        closeButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                menu.setWordoku(wordokuCheckBox.isSelected());
-                dispose();
-            }
+        closeButton.addActionListener(click -> {
+            menu.setWordoku(wordokuCheckBox.isSelected());
+            setVisible(false);
         });
 
-
-
-
-
         add(panel);
-        setVisible(true);
         setSize(300,300);
         setLocationRelativeTo(null);
-        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setDefaultCloseOperation(HIDE_ON_CLOSE);
+    }
 
+    public void showWindow() {
+        setVisible(true);
     }
 
 
