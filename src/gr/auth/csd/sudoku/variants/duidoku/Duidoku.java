aa74394c25a5653ca5931dde6cdd340ee48b21a8
lomvardo <lomvardo@csd.auth.gr>
2020-01-06 18:16:38 +0200

    Added Computer Move

--- src/gr/auth/csd/sudoku/variants/duidoku/Duidoku.java
+++ src/gr/auth/csd/sudoku/variants/duidoku/Duidoku.java
@@ -2,6 +2,8 @@ package gr.auth.csd.sudoku.variants.duidoku;
 
 import gr.auth.csd.sudoku.variants.classic.ClassicSudoku;
 
+import java.util.Random;
+
 public class Duidoku extends ClassicSudoku {
     /**
      * Creates a new Duidoku from a given size.
@@ -23,5 +25,19 @@ public class Duidoku extends ClassicSudoku {
             }
         }
         return true;
+    }
+
+    public void computerMove() {
+        Random random = new Random();
+        int row, col, value;
+
+        do {
+            row = random.nextInt(gridSize);
+            col = random.nextInt(gridSize);
+        } while (grid[row][col] != 0);
+
+        do {
+            value = 1 + random.nextInt(gridSize);
+        } while(!isValidMove(row, col, value));
     }
 }
