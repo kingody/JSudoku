23148a2481c6140a88dbdc3d2461c4bb7585449a
lomvardo <lomvardo@csd.auth.gr>
2020-01-10 19:47:12 +0200

    Added tests

---
+++ tests/gr/auth/csd/sudoku/variants/DuidokuTest.java
@@ -0,0 +1,16 @@
+package gr.auth.csd.sudoku.variants;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DuidokuTest {
+
+    @Test
+    void isCompleted() {
+    }
+
+    @Test
+    void computerMove() {
+    }
+}
