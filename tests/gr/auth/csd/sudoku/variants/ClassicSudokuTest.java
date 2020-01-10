121f86ab12924e6487c1211cbed5dac31807250b
alex <alexsofo@csd.auth.gr>
2020-01-10 20:00:06 +0200

    Test setCell

--- tests/gr/auth/csd/sudoku/variants/ClassicSudokuTest.java
+++ tests/gr/auth/csd/sudoku/variants/ClassicSudokuTest.java
@@ -1,12 +1,19 @@
 package gr.auth.csd.sudoku.variants;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClassicSudokuTest {
+    private ClassicSudoku sud;
+    @BeforeEach
+    void setUp(){
+       sud = new ClassicSudoku(9,"classic1.txt");
+    }
 
     @Test
-    void isValidMove() {
+    void setCell() {
+        assertTrue(sud.setCell(2,0,5));
     }
 }
