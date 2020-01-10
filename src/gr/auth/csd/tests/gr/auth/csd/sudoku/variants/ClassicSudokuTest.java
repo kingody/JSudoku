3e6024527b00e09291c10b04eb2791a4924e7f34
alex <alexsofo@csd.auth.gr>
2020-01-10 19:39:33 +0200

    basic Tests

---
+++ src/gr/auth/csd/tests/gr/auth/csd/sudoku/variants/ClassicSudokuTest.java
@@ -0,0 +1,12 @@
+package gr.auth.csd.sudoku.variants;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClassicSudokuTest {
+
+    @Test
+    void isValidMove() {
+    }
+}
